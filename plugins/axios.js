2ea5e68c4309d05f48359c9374d7d0b17173a520
Mercutio <mercutio_john@foxmail.com>
2023-04-29 22:56:08 +0800

    error handle

--- plugins/axios.js
+++ plugins/axios.js
@@ -1,11 +1,11 @@
-// export default function({ $axios, error: nuxtError }) {
-//   $axios.onError(error => {
-//     // nuxtError({
-//     //   statusCode: error.response.status,
-//     //   message: error.message
-//     // });
-//     console.error(error);
-//     // return Promise.resolve(false);
-//     return Promise.resolve(error);
-//   });
-// }
+export default function({ $axios, error: nuxtError }) {
+    $axios.onError(error => {
+        nuxtError({
+            statusCode: error.response.status,
+            message: error.message
+        });
+        console.error(error);
+        return Promise.resolve(false);
+        // return Promise.resolve(error);
+    });
+}
