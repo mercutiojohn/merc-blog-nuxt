75a64f4008142bd508e5297206318170c874e2a5
mercutio <mercutio_john@foxmail.com>
2023-08-04 00:33:33 +0800

    ok

--- components/utils/Block.vue
+++ components/utils/Block.vue
@@ -11,9 +11,6 @@
 </template>
 
 <script>
-import { gsap } from "gsap";
-import { ScrollTrigger } from "gsap/ScrollTrigger";
-
 export default {
   name: "Block",
   props: {
@@ -25,40 +22,6 @@ export default {
       type: Boolean,
       default: false
     }
-  },
-  mounted(){
-    gsap.registerPlugin(ScrollTrigger); // 注册 ScrollTrigger 插件
-    const titleEffect = this.$refs.titleEffect;
-    const fl = gsap.timeline({
-      scrollTrigger: {
-        trigger: titleEffect,
-        scrub: .8,
-        // markers: true,
-        start: "top 90%", // 动画开始位置
-        end: "top 90%", // 动画结束位置
-        toggleActions: "play none none reverse" // 动画结束时反转
-      }
-    });
-    fl.from(titleEffect, { 
-      opacity: 0, 
-      duration: 0.5,
-      transform: "scale(.8) translateY(20px)"
-     });
-    // const tl = gsap.timeline({
-    //   scrollTrigger: {
-    //     trigger: titleEffect,
-    //     scrub: .8,
-    //     // markers: true,
-    //     start: "top 10%", // 动画开始位置
-    //     end: "top 4%", // 动画结束位置
-    //     toggleActions: "play none none reverse" // 动画结束时反转
-    //   }
-    // });
-    // tl.to(titleEffect, { 
-    //   opacity: 0, 
-    //   duration: 0.5,
-    //   transform: "scale(.8) translateY(20px)"
-    //  });
   }
 };
 </script>
